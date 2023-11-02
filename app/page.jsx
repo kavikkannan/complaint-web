d2809f5a0b81999e3a8c6435b2a44f49205c041d
subi <rsubikksha@gmail.com>
2023-11-02 12:56:59 +0530

    the final2

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Header_LPage from '@/components/Header_LPage';
 import Body_Lpage from '@/components/Body_Lpage';
-
+import React from 'react';
 export default function Home() {
   return (
     <div> 
